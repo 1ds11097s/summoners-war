3645c9cdfaf3aaf7b7deccde1115d9d9b02b3ff6
大坪 裕二 <1ds11097s@gmail.com>
2020-04-26 15:53:22 +0900

    削除ファイル復元

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-img
+    :src="headerImage"
+    max-height="200"
+  />
+</template>
+<script>
+import HeaderImage from "@/assets/image/header.webp"
+export default {
+  data() {
+    return {
+      headerImage: HeaderImage,
+    }
+  }
+}
+</script>
